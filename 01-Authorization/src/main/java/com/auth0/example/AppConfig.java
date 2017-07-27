4463bdb51cba2ce5ee99dd88fc6c2aae78c4409c
Yaroslav Yermilov <yermilov17@gmail.com>
2017-07-28 02:20:05 +0300

    follow Josef instructions

--- 01-Authorization/src/main/java/com/auth0/example/AppConfig.java
+++ 01-Authorization/src/main/java/com/auth0/example/AppConfig.java
@@ -1,12 +1,17 @@
 package com.auth0.example;
 
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import com.google.common.collect.ImmutableList;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -25,11 +30,24 @@ public class AppConfig extends WebSecurityConfigurerAdapter {
                 .configure(http)
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/login").permitAll()
-                .antMatchers(HttpMethod.GET, "/photos/**").hasAuthority("read:photos")
-                .antMatchers(HttpMethod.POST, "/photos/**").hasAuthority("create:photos")
-                .antMatchers(HttpMethod.PUT, "/photos/**").hasAuthority("update:photos")
-                .antMatchers(HttpMethod.DELETE, "/photos/**").hasAuthority("delete:photos")
-                .anyRequest().authenticated();
+                .antMatchers(HttpMethod.GET, "/photos/**").hasAuthority("get:sections")
+                .anyRequest().authenticated()
+                .and().cors();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of(""));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
